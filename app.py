49d283806f15c16ec906d3129e50a0dcd8e96a32
Theodore Ogonah <theoogonah@gmail.com>
2023-09-29 23:23:23 +0100

    getting the postgres sql database to work in time to realise my effort was a complete waste of time

--- app.py
+++ app.py
@@ -9,15 +9,12 @@ app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///players.db"
 app.app_context().push()
 db=SQLAlchemy(app)
 
-need to install psycopg2
-
-
 class Players(db.Model):
-    id=db.Column(db.Float, primary_key=True) 
-    name=db.Column(db.String(50),  unique=True)
-    pos=db.Column(db.String(2), nullable=False)
-    age=db.Column(db.Float, nullable=False)
-    tm=db.Column(db.String(3), nullable=False)
+    id=db.Column(db.Float, primary_key=True, autoincrement=True, nullable=True) 
+    name=db.Column(db.String(50))
+    pos=db.Column(db.String(2))
+    age=db.Column(db.Float)
+    tm=db.Column(db.String(3))
     g=db.Column(db.Float)
     gs=db.Column(db.Float)
     mp=db.Column(db.Float)
@@ -47,32 +44,32 @@ class Players(db.Model):
 
     def __repr__(self):
         return '<Name %r>' %self.name
+    
 with app.app_context():
     db.create_all()
-    
-    
-    
     
     
 class ReadCsv:
     def __init__(self) -> None:
         base_path=os.path.normpath(os.getcwd()+ os.sep+ os.pardir)
         df_path=f"{base_path}{os.sep}nba-flask{os.sep}static{os.sep}csv"
+        global temp_df
         temp_df=pandas.read_csv(f"{df_path}{os.sep}2021.csv")
         for i in range(0, len(temp_df["Player"])):
-            entry=Players(name=str(temp_df.iloc[i, 0]), pos=str(temp_df.iloc[i, 1]), age=float(temp_df.iloc[i, 2]),
-                          tm=str(temp_df.iloc[i, 3]), g=float(temp_df.iloc[i, 4]), gs=float(temp_df.iloc[i, 5]), mp=float(temp_df.iloc[i, 6]),
-                          fg=float(temp_df.iloc[i, 7]),fga=float(temp_df.iloc[i, 8]), fg_percent=float(temp_df.iloc[i, 9]),
-                          three_point=float(temp_df.iloc[i, 10]), three_point_a=int(temp_df.iloc[i, 11]), three_point_percent=int(temp_df.iloc[i, 12]),
-                          two_point=float(temp_df.iloc[i, 13]), two_point_a=float(temp_df.iloc[i, 14]), two_point_percent=float(temp_df.iloc[i, 15]),
-                          efg=float(temp_df.iloc[i, 16]), ft=float(temp_df.iloc[i, 17]), fta=float(temp_df.iloc[i, 18]), ft_percent=float(temp_df.iloc[i, 19]),
-                          orb=float(temp_df.iloc[i, 20]), drb=float(temp_df.iloc[i, 21]), trb=float(temp_df.iloc[i, 22]),
-                          ast=float(temp_df.iloc[i, 23]), stl=float(temp_df.iloc[i, 24]), blk=float(temp_df.iloc[i, 25]),
-                          tov=float(temp_df.iloc[i, 26]), pf=float(temp_df.iloc[i, 27]), pts=float(temp_df.iloc[i, 28]))
+            entry=Players(
+                name=str(temp_df.iloc[i, 0]), pos=str(temp_df.iloc[i, 1]), age=float(temp_df.iloc[i, 2]),
+                tm=str(temp_df.iloc[i, 3]), g=float(temp_df.iloc[i, 4]), gs=float(temp_df.iloc[i, 5]),
+                mp=float(temp_df.iloc[i, 6]), fg=float(temp_df.iloc[i, 7]),fga=float(temp_df.iloc[i, 8]),
+                fg_percent=float(temp_df.iloc[i, 9]), three_point=float(temp_df.iloc[i, 10]), three_point_a=float(temp_df.iloc[i, 11]),
+                three_point_percent=float(temp_df.iloc[i, 12]), two_point=float(temp_df.iloc[i, 13]), two_point_a=float(temp_df.iloc[i, 14]),
+                two_point_percent=float(temp_df.iloc[i, 15]),efg=float(temp_df.iloc[i, 16]), ft=float(temp_df.iloc[i, 17]), 
+                fta=float(temp_df.iloc[i, 18]), ft_percent=float(temp_df.iloc[i, 19]),orb=float(temp_df.iloc[i, 20]),
+                drb=float(temp_df.iloc[i, 21]), trb=float(temp_df.iloc[i, 22]), ast=float(temp_df.iloc[i, 23]),
+                stl=float(temp_df.iloc[i, 24]), blk=float(temp_df.iloc[i, 25]), tov=float(temp_df.iloc[i, 26]), 
+                pf=float(temp_df.iloc[i, 27]), pts=float(temp_df.iloc[i, 28]))
             db.session.add(entry)
             db.session.commit()
 instance=ReadCsv()
-
 
 @app.route("/")
 
@@ -87,7 +84,9 @@ def page_not_found(e):
 @app.route("/players")
 
 def players():
-    return render_template("players.html")
+    
+    players_table=Players.query.order_by(Players.name)
+    return render_template("players.html", players_table=[temp_df.to_html()], header=True)
 
 
 
